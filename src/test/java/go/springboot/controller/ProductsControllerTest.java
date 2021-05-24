f9b68715e6f157e585cbf96301d69a098cd60230
glenn opdycke-hansen <glennoph@gmail.com>
2021-05-24 10:49:40 -0500

    add test ProductsControllerTest WIP jsonPath to check test results

--- src/test/java/go/springboot/controller/ProductsControllerTest.java
+++ src/test/java/go/springboot/controller/ProductsControllerTest.java
@@ -1,20 +1,28 @@
 package go.springboot.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import go.springboot.entity.Product;
 import go.springboot.service.ProductService;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.mockito.ArgumentMatchers.contains;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
 
 import static java.util.UUID.*;
 import static org.mockito.ArgumentMatchers.any;
@@ -23,7 +31,10 @@ import static org.mockito.BDDMockito.given;
 @ExtendWith(MockitoExtension.class)
 class ProductsControllerTest {
 
+    public static final String API_PRODUCTS = "/api/products/";
     private String randomid = randomUUID().toString();
+    private String addedid = randomUUID().toString();
+
     @Mock
     ProductService productService;
 
@@ -32,10 +43,13 @@ class ProductsControllerTest {
 
     MockMvc mockMvc;
 
+
     Product testProduct;
+    Product addedProduct;
 
     @BeforeEach
     void setUp() {
+
         testProduct = Product.builder()
                 .id(randomid)
                 .name("test-product")
@@ -43,8 +57,24 @@ class ProductsControllerTest {
                 .category("testing")
                 .build();
 
-        mockMvc = MockMvcBuilders.standaloneSetup(productsController)
+        addedProduct = Product.builder()
+                .id(addedid)
+                .name("test-product-added")
+                .type("test")
+                .category("testing")
                 .build();
+
+        List<Product> productList = new ArrayList<>();
+        productList.add(addedProduct);
+
+        mockMvc = MockMvcBuilders.standaloneSetup(productsController).build();
+
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.out.println("tearDown todo");
+        //reset(productService);
     }
 
 
@@ -53,12 +83,39 @@ class ProductsControllerTest {
     void getProduct() throws Exception {
         given(productService.getProduct(any())).willReturn(testProduct);
 
-        mockMvc.perform(get("/api/products/"+randomid))
-        .andExpect(status().isOk());
+        mockMvc.perform(get(API_PRODUCTS+randomid))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(jsonPath("$.id", is(randomid))) // check id
+                .andExpect(jsonPath("$.id", is(testProduct.getId()))) // check id from test product
+                .andExpect(jsonPath("$.name", is(testProduct.getName()))) // check name from test product
+                .andExpect(jsonPath("$.type", is(testProduct.getType()))) // check type from test product
+                .andDo(MockMvcResultHandlers.print()) // print result data
+        ;
     }
 
     @Test
-    void saveProduct() {
+    @Disabled("fail")
+    void saveProduct() throws Exception {
+        //NB test failed because product was not added
+        Product updatedProduct = Product.builder()
+                .id(randomid)
+                .name("updated-product")
+                .type("test-save").category("testing")
+                .build();
+
+        given(productService.save(any()))
+        .willReturn(updatedProduct);
+
+        String updatedProductString = new ObjectMapper().writeValueAsString(updatedProduct);
+        System.out.println("updatedProductString="+updatedProductString);
+        mockMvc.perform(post(API_PRODUCTS)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(updatedProductString))
+                .andExpect(status().isOk())
+                .andDo(MockMvcResultHandlers.print())
+                ;
+
     }
 
     @Test
